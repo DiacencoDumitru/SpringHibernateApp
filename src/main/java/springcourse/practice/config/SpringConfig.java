c9db5eb2054dc06164f51f8ee3b43991d476c7d2
Dumitru <119630796+DiacencoDumitru@users.noreply.github.com>
2024-12-12 14:49:59 +0200

    update

--- src/main/java/springcourse/practice/config/SpringConfig.java
+++ src/main/java/springcourse/practice/config/SpringConfig.java
@@ -96,7 +96,7 @@ public class SpringConfig implements WebMvcConfigurer {
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("ru.alishev.springcourse.models");
+        sessionFactory.setPackagesToScan("springcourse.practice.models");
         sessionFactory.setHibernateProperties(hibernateProperties());
 
         return sessionFactory;
